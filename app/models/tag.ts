dbc44481aa388d755c1cd643b86f371ed3d44615
zgeniusecoders <zgeniusecoders@gmail.com>
2025-11-03 19:41:00 +0100

    feat(tag): ajout de la relations posts dans le modele

--- app/models/tag.ts
+++ app/models/tag.ts
@@ -1,5 +1,7 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, column, manyToMany } from '@adonisjs/lucid/orm'
+import * as relations from '@adonisjs/lucid/types/relations'
+import Post from '#models/post'
 
 export default class Tag extends BaseModel {
   @column({ isPrimary: true })
@@ -7,6 +9,9 @@ export default class Tag extends BaseModel {
 
   @column()
   declare name: string
+
+  @manyToMany(() => Post, { pivotTable: 'post_tag' })
+  declare posts: relations.ManyToMany<typeof Post>
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime

5405e323bc23e2552fd22cb2cfa6d7128b640438
zgeniusecoders <zgeniusecoders@gmail.com>
2025-11-05 23:30:34 +0100

    feat: ajout de la relations posts dans le modele user.ts

--- app/models/user.ts
+++ app/models/user.ts
@@ -1,9 +1,11 @@
 import { DateTime } from 'luxon'
 import hash from '@adonisjs/core/services/hash'
 import { compose } from '@adonisjs/core/helpers'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
 import { DbAccessTokensProvider } from '@adonisjs/auth/access_tokens'
+import Post from '#models/post'
+import * as relations from '@adonisjs/lucid/types/relations'
 
 const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
   uids: ['email'],
@@ -19,6 +21,9 @@ export default class User extends compose(BaseModel, AuthFinder) {
 
   @column()
   declare email: string
+
+  @hasMany(() => Post)
+  declare posts: relations.HasMany<typeof Post>
 
   @column({ serializeAs: null })
   declare password: string

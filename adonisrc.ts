88ab188529319baaa064eefdb4b0236fd364fc13
zgeniusecoders <zgeniusecoders@gmail.com>
2025-11-06 07:27:39 +0100

    chore: installation du bouncer pour gerer les  authorisations

--- adonisrc.ts
+++ adonisrc.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   | will be scanned automatically from the "./commands" directory.
   |
   */
-  commands: [() => import('@adonisjs/core/commands'), () => import('@adonisjs/lucid/commands')],
+  commands: [() => import('@adonisjs/core/commands'), () => import('@adonisjs/lucid/commands'), () => import('@adonisjs/bouncer/commands')],
 
   /*
   |--------------------------------------------------------------------------
@@ -46,7 +46,8 @@ export default defineConfig({
     () => import('@adonisjs/core/providers/vinejs_provider'),
     () => import('@adonisjs/cors/cors_provider'),
     () => import('@adonisjs/lucid/database_provider'),
-    () => import('@adonisjs/auth/auth_provider')
+    () => import('@adonisjs/auth/auth_provider'),
+    () => import('@adonisjs/bouncer/bouncer_provider')
   ],
 
   /*
